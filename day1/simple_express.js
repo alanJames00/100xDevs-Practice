acb156c1db044fbe11fd5a09927a02c1460b4441
alanjames00 <alanjames6282@gmail.com>
2023-10-03 19:48:17 +0000

    add bodyparser

--- day1/simple_express.js
+++ day1/simple_express.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const bodyParser = require('body-parser');
 const calculateSum = require('./model');
 
+
+app.use(bodyParser.json());
+
 app.get('/', (req ,res)=>{
-    const n = req.query.n;
-    console.log(n);
     
+    const count = req.body.counter;
     res.json({
-        "input" : n,
-        "sum" : calculateSum(n)
+        'counter':count
     })
 });
 
@@ -24,6 +25,7 @@ function handlePostOne(req, res){
     // There are multiple ways to input data using post ->
     // 1. using query params ?
     // 2. using headers
+    // 3. using body
 
     const counter = req.header('counter'); // used for accessing the header from a http request
                                             // where the header is named counter
